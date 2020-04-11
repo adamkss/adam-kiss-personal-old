a6010dc0fcf781213e3a6edca4abc6f25dc19ceb
Adam Kiss <kiss.adam.a@gmail.com>
2020-04-11 21:52:12 +0300

    Modify netlify cms

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,15 +3,44 @@ import SEO from "../components/seo"
 import Header from "../components/header/header";
 import styles from './portfolio.module.css';
 import { Classnames } from '../utils/ClassNameUtils';
+import { graphql } from 'gatsby';
 
-const Portfolio = () => (
-  <>
-    <SEO title="Portfolio" />
-    <Header switchToBlackTextDelayed />
-    <main className={Classnames(styles.main)}>
+const Portfolio = ({ data }) => {
+  const posts = data.allMdx.edges;
 
-    </main>
-  </>
-)
+  return (
+    <>
+      <SEO title="Portfolio" />
+      <Header switchToBlackTextDelayed />
+      <main className={Classnames(styles.main)}>
 
+      </main>
+    </>
+  )
+}
 export default Portfolio
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
